d9a05bfce8674a1a60c45c61df67aa91c92e9978
Tony-bytes-0 <79365044+Tony-bytes-0@users.noreply.github.com>
2022-06-28 08:23:05 -0500

    Add files via upload

--- characterObjects.py
+++ characterObjects.py
@@ -1,5 +1,5 @@
 from asyncio.windows_events import NULL
-from  data import itemList, skillList, tecniqueList
+from  data import itemList, skillList, tecniqueList, statList
 import time, random
 from os import system
 class character():
@@ -35,7 +35,7 @@ class character():
     def wait(self):
         print(self.name,'se calma por un momento, recuperando energia')
         self.addSomeStat('energy', (self.maxEnergy * 0.5))
-        self.addSomeStat('mana' ,(self.maxMana * 0.2))
+        self.addSomeStat('mana' ,(self.maxMana * 0.3))
         return False
     
     def guard(self):
@@ -54,18 +54,18 @@ class character():
 
     def calculateDamage(self, dmg,  attacker):
         r = random.randint(0, 99)
-        if 'En Guardia' in self.states:#reducir daño 50 %
-            dmg = dmg * 0.5
-        elif 'Cansado' in self.states:#aumentar daño en 20 %
-            dmg = dmg * 1.2
+        if 'En Guardia' in self.states:#reducir daño 60 %
+            dmg = dmg * 0.6
+        elif 'Cansado' in self.states:#aumentar daño en 50 %
+            dmg = dmg * 1.5
         if dmg < 1:
             dmg = 1
         #--------------- calculo de evasion ---------------------
-        evadeProb = 10 + (attacker.speed - self.speed)
+        evadeProb = 2 + (attacker.speed - self.speed)
         print('evade prob:',evadeProb,'random int',r)#depuracion
         if evadeProb >= r:
             print(self.name,' ha evitado el ataque!!!')#screen
-         #--------------- calculo de evasion ---------------------
+
         else:
             print(self.name,' recibe  >',round(dmg, 2),'<  puntos de daño!!!')#screen
             self.life = self.life - dmg
@@ -112,9 +112,14 @@ class character():
         self.activeStates()
 
     def addState(self, state):
+        print('estado antes de agregar estados puta madre', self.states)
         if state != NULL:
             self.states.append(state)
             print(self.name,' esta: ',state)
+        if state == 'Debilitado':#debo terminar el metodo export
+            pass
+
+
     def activeStates(self):#escanea el objeto y segun ciertas condiciones, evalua condiciones de debuffs
         if self.energy <= self.maxEnergy * 0.25 and 'Cansado' not in self.states:
             self.addState('Cansado')
@@ -129,6 +134,17 @@ class character():
         for x in listOfOneTurnBuffs:#recorrerlos
             if x in self.states:#si existe alguno al momento de iniciar el turno, eliminarlo
                 self.states.remove(x)
+            
+    def resetToInitialState(self, maxLife, att, deff, speed, maxMana, maxEnergy):
+        self.maxLife = maxLife
+        self.attack = att
+        self.defence = deff
+        self.speed = speed
+        self.maxMana = maxMana
+        self.maxEnergy = maxEnergy
+
+
+
 # ------------------ magias
     def tecniqueSumary(self, select, defender):#recibe un str con el nombre de la skill y la ejecuta
         totalDamage = 0
@@ -143,6 +159,8 @@ class character():
             print('Enfocando su energia en la palma de su mano: ',self.name,' Dispara una Rafaga de Viento!')#screen
             defender.calculateDamage(damage, self)
             self.addSomeStat('mana', -9)
+
+
 # ------------------ Tecnicas
         elif select == 'Doble Golpe':
             damage = ((self.attack * 0.5) - (defender.defence * 0.5)  +   1)
@@ -150,7 +168,19 @@ class character():
             defender.calculateDamage(damage, self)
             print('para rematar con un segundo golpe!  ', end='')
             defender.calculateDamage(damage, self)
+
+        elif select == 'Punto Ciego':
+            damage = ((self.attack * 0.5) - (defender.defence * 0.1) + 1 )
+            print(self.name, ' realiza un golpe rapido, tomando fuera de guardia a su rival  ' , end='')
+            defender.calculateDamage(damage, self)
+
+        elif select == 'Golpe Bajo':
+            damage = ((self.attack * 0.3) - (defender.defence * 0.4) + 1 )
+            print(self.name, ' Dispara una flecha apuntando a los puntos de apoyo del rival ' , end='')
+            defender.calculateDamage(damage, self)
+            defender.addSomeStat()
         
+
         elif select == 'Tornado Infernal':
             print(self.name, ' Empuña sus armas y comienza un frenesi de giros incesantes que cortan contra ',
             defender.name,'\ntratando de conectar repetidos impactos')
@@ -162,7 +192,7 @@ class character():
                 
 
         else:
-            print('no paso nada aqui en el metodo de usar skills uwu')
+            print('no paso nada aqui en el metodo de usar skills uwu')#depuracion
            
         if select in ['Rafaga de Viento', 'Golpe Chispa']:#skills que restan uno de energia
             self.addSomeStat('energy', -1)
@@ -171,7 +201,7 @@ class character():
             self.addSomeStat('energy', -5)
         
 # ------------------ uso de skills
-    def skillsAndTecniquesAvalible(self):#comprueba si el objeto posee tecnicas asi como sus requerimientos de mana
+    def skillsAndTecniquesAvalible(self, showSkills):#comprueba si el objeto posee tecnicas asi como sus requerimientos de mana
         all = {}
         all.update(skillList)
         all.update(tecniqueList)
@@ -180,32 +210,27 @@ class character():
         for x in all.keys():
             #print('habilidad de turno: ', x) a veces quiero imprimir las skill al recorrerlas xd
             if x in self.skills and self.mana >= skillList[str(x)]:#condiciones de uso
-                print(i ,'._', x)
+                if showSkills:
+                    print(i ,'._', x)
                 ready.append(x)
-                i = i + 1
+
 
             elif x in self.tecniques and self.energy >= tecniqueList[str(x)]:#condiciones de uso
                 print(i ,'._', x)
                 ready.append(x)
-                i = i + 1
 
         if ready != []:
             return ready
         
         else:
-            return []
-
-
-    def tecniquesAvalible(self):
-        ready = self.skillsAndTecniquesAvalible()
-        i = 0
-        for x in tecniqueList.keys():
-            if x in self.tecniques and self.energy >= tecniqueList[str(x)]:#condiciones de uso
-                print(i ,'._', x)
-                ready.append(x)
+            return False
 
     def useSkill(self, defender):#disponible en el menu del jugador
-        ready = self.skillsAndTecniquesAvalible()
+        ready = self.skillsAndTecniquesAvalible(True)
+        if ready == False:
+            print('En Terminos de Habilidades, no Hay Habilidades Disponibles')
+            return True
+
         try:
             op = int(input('\n-->'))
             self.tecniqueSumary(ready[op], defender)
@@ -240,51 +265,49 @@ class mainCharacter(character):#mc personaje principal
 
         if self.exp >= self.maxExp:# --- subir de nivel ---
             print('------------------- Subes de Nivel! -------------------')
-            self.addStat( 2 )
-            self.maxExp = self.maxExp + (self.maxExp * 0.30)
-            self.exp = 0
+            amount = 4
 
-    def addStat(self, amount):# --- aqui es donde se agregan las estadisticas ---
-        def changueAtt(self, attribute, skillPoints):
-            if attribute == 0:
-                self.life = self.life +  skillPoints
-                self.maxLife = self.maxLife +  skillPoints
-            elif attribute == 1:
-                self.attack = self.attack +  skillPoints
-            elif attribute == 2:
-                self.defence =self.defence +  skillPoints
-            elif attribute == 3:
-                self.speed =self.speed + skillPoints 
-            elif attribute == 4:
-                self.maxEnergy =self.maxEnergy + skillPoints 
-            elif attribute == 5:    
-                self.maxMana =self.maxMana + skillPoints    
+            while amount > 0:
+                i = 0
+                for stat in statList:#Screen
+                    print(i,'._ ' , stat)
+                    i = i+1
+                print('puntos disponibles: ', amount)
+                try:
+                    attribute = int(input('caracteristica a mejorar? -->'))
+                    attribute = statList[attribute]
+                    skillPoints = int(input('cantidad a agregar? -->'))
+                    amount = amount - skillPoints
+                except:
+                    print('alguno de los datos introducidos no fue valido :(')
+                
 
-        while amount > 0:#bucle para agregar puntos
-             stats = [ 'Vida', 'Ataque', 'Defensa', 'Velocidad', 'Energia','Mana']
-             i = 0
-             for stat in stats:#mensaje en pantalla
-                 print(i,'._ ' , stat)
-                 i = i+1
+                self.addStat(attribute,  skillPoints )#trabajando aqui
+                self.maxExp = self.maxExp + (self.maxExp * 0.30)
+                self.exp = 0
 
-             try:
-                 attribute = int(input('caracteristica a mejorar? -->'))
-                 print('puntos disponibles: ', amount)
-                 skillPoints = int(input('cantidad a agregar? -->'))
+    def addStat(self, attribute, amount):# --- aqui es donde se agregan las estadisticas ---
 
-                 if skillPoints <= amount:#agregar skillPoints
-                     changueAtt(self, attribute, skillPoints)
-                     amount = amount - skillPoints
+        if attribute == 'Vida':
+            print(self.life, attribute, amount)
+            self.life = self.life +  amount
+            self.maxLife = self.maxLife +  amount
+        elif attribute == 'Ataque':
+            self.attack = self.attack +  amount
+        elif attribute == 'Defensa':
+            self.defence =self.defence +  amount
+        elif attribute == 'Velocidad':
+            self.speed =self.speed + amount 
+        elif attribute == 'Energia':
+            self.maxEnergy =self.maxEnergy + amount 
+        elif attribute == 'Mana':    
+            self.maxMana =self.maxMana + amount
+        else:
+            print('>>> datos introducidos erroneamente al subir stats <<<', attribute, amount)
+           
 
-                 else:
-                     print('introduzca un numero entre 0 y ', amount)
-
-             except TypeError or NameError or ValueError:
-                 print(' algo salio mal :( ')
-             except :
-                print('ocurrio un error no capturable')
-    
-    def reStat(self, na, li, maxLi, en, maxEn, exp, maxExp, att, de, sp, chClass):# cambios en las estadisticas
+ 
+    def reStat(self, na, li, maxLi, en, maxEn, exp, maxExp, att, de, sp, chClass, mana, maxMana):# cambios en las estadisticas
         self.name = na
         self.life = li
         self.maxLife = maxLi
@@ -296,6 +319,8 @@ class mainCharacter(character):#mc personaje principal
         self.defence = de
         self.speed = sp
         self.chClass = chClass
+        self.mana = mana
+        self.maxMana = maxMana
     
     def showInventory(self):
         avalibleItems = []
@@ -334,8 +359,8 @@ class mainCharacter(character):#mc personaje principal
 
     def turn(self,enemy):#------------------------------------aqui esta el menu del turno del personaje------------------
         loop=True
-        menu = ['\n0._ limpiar consola','\n1._ Atacar','\n2._ Guardia',
-        '\n3._ Habilidades' ,'\n4._Estado','\n5._ Esperar','\n6._ Objetos']
+        menu = ['0._ limpiar consola','1._ Atacar','2._ Guardia',
+        '3._ Habilidades' ,'4._Estado','5._ Esperar','6._ Objetos']
         self.cleaner()
         if self.energy <= 1:
             print(self.name,' esta demasiado cansado para actuar')
@@ -343,9 +368,8 @@ class mainCharacter(character):#mc personaje principal
 
         while loop:
             time.sleep(0.3)#delay
-            for options in menu:
-                print(options)
-            op = input('\n---> ')
+            print()
+            op = input(str(menu)+'\n---> ')
             print('\n')
             if op =='0':
                 system('cls')
@@ -366,41 +390,28 @@ class mainCharacter(character):#mc personaje principal
                 print('\n opcion invalida. papuh')
             self.activeStates()
     # ----------- no utilizable dentro del juego --------------
-    def exportData(self, adventure):
-        exportedEnemys = ",".join(adventure.avalibleEnemys)
-        world = adventure.world
-        return tuple([self.name, self.life, self.maxLife, self.energy, self.maxEnergy,
+    def export(self):#no he terminado esto, trabajando aqui
+        allSkills = " ".join(self.skills)
+
+        print(allSkills)
+
+        allSkills = " ".join(self.tecniques)
+        print(allSkills)
+        '''return tuple([self.name, self.life, self.maxLife, self.energy, self.maxEnergy,
 		self.exp, self.maxExp,self.attack, self.defence, self.speed, self.chClass,
-        adventure.level, world, exportedEnemys, adventure.days])
+        adventure.level, world, exportedEnemys, adventure.days])'''
+
+    def removeStatusAfterBattle(self):
+        listOfBattleAlongBuffs =['Debilitado']
+        for x in listOfBattleAlongBuffs:#recorrer
+            if x in self.states:#si existe alguno al final del combate, eliminarlo
+                self.states.remove(x)
 
 class enemy(character):
     def __init__(self,na,li,att,de,sp,en, ap, mana, skills, tecniques):
         super().__init__(na,li,att,de,sp, en, mana, skills, tecniques)
         self.aptitude = ap
-    #clases mutadas--------------
 
-    def skillsAndTecniquesAvalible(self):#comprueba si el objeto posee tecnicas asi como sus requerimientos de mana
-        all = {}
-        all.update(skillList)
-        all.update(tecniqueList)
-        ready = []
-        i = 0
-        for x in all.keys():
-            if x in self.skills and self.mana >= skillList[str(x)]:#condiciones de uso
-                ready.append(x)
-                i = i + 1
-
-            elif x in self.tecniques and self.energy >= tecniqueList[str(x)]:#condiciones de uso
-                ready.append(x)
-                i = i + 1
-
-        if ready != []:
-            return ready
-        
-        else:
-            return []
-
-    #clases mutadas--------------
     def reStat(self,na,li,att,de,sp,en, ap, mana, skills, tecniques):
         self.name = na
         self.life = li
@@ -416,6 +427,7 @@ class enemy(character):
         self.aptitude = ap  #comportamiento del enemigo: 
         self.skills = skills
         self.tecniques = tecniques
+        #self.states = []
 
         #items extra:
         if self.name == 'goblin':  
@@ -423,6 +435,7 @@ class enemy(character):
 
     def turn(self, player):
         op = random.randint(0,9)#tirada de dados del 0 al 9, basicamente
+        print('esta es la tirada de dados del turno del enemigo:',op)
         time.sleep(0.3)
 
         if 'dumb' in self.aptitude:# 90% atacar 10% esperar
@@ -445,33 +458,31 @@ class enemy(character):
 
                 elif op == 9 and self.energy < (self.maxEnergy * 0.9):
                     self.useItem('pocion de energia')
+
                 else:
                     self.basicAttack(player)
             else:
                 self.wait()
 #-----------------------------------------------------------
-        elif 'magicNormal' in self.aptitude:# 70% tratar de lanzar skills 
-            if self.energy >= 1 and self.skillsAndTecniquesAvalible() != False :#mientras halla 1 energia y skill disponible
-                if op <= 6:
-                    self.tecniqueSumary(random.choice(self.skills), player) #usar skill aleatoria
+        elif 'magicNormal' in self.aptitude:# 60% tratar de lanzar skills / curarse
+            if self.energy >= 1:#mientras halla 1 de energia
+                if op <= 7 and self.skillsAndTecniquesAvalible(False) != False:
+                    self.tecniqueSumary(random.choice(self.skillsAndTecniquesAvalible(False)), player) #usar skill aleatoria
                    
                 elif op <= 7 and self.life <= (self.maxLife * 0.5):
                     self.useItem('pocion de energia')
                 
-                elif op == 8:
+                else:
                     self.guard()
-
-            elif self.energy >= 2:
-                self.basicAttack(player)
 
             else:
                 self.wait()
 #----------------------------------------------------------- 
         elif 'great' in self.aptitude:# 40% skill - 30% basico - 20% guardia - 10% curarse (si falta vida)
             print(op)
-            if self.energy >= 1 and self.skillsAndTecniquesAvalible() != False :#mientras halla 1 energia y skill disponible
+            if self.energy >= 1 and self.skillsAndTecniquesAvalible(False) != False :#mientras halla 1 energia y skill disponible
                 if op <= 3:
-                    self.tecniqueSumary(random.choice(self.skillsAndTecniquesAvalible()), player) #usar skill aleatoria
+                    self.tecniqueSumary(random.choice(self.skillsAndTecniquesAvalible(False)), player) #usar skill aleatoria
                    
                 elif op <= 6  and self.life >= (self.maxLife * 0.5):
                     self.basicAttack(player)
--- data.py
+++ data.py
@@ -1,10 +1,17 @@
 preClass=[('Life','Attack','Defence','Speed','Magic Power'),
 (10, 3, 5, 1,'Guerrero', 6, 1 , [], ['Doble Golpe']),
+
 (6, 1, 2, 5,'Maga', 5, 10, ['Golpe Chispa'], []),
-(5, 6, 1, 7,'Asesina', 6, 4, []), [],
+
+(5, 6, 1, 7,'Asesina', 6, 4, [], ['Punto Ciego']),
+
 (15, 2, 2, 3,'No Muerto', 4, 8, [], []),
-(6, 5, 5, 3,'Cazador', 10, 3, [], []),
-(100, 20, 10, 10, 'admin', 100, 10, [], [])]
+
+(6, 5, 5, 3,'Cazador', 10, 1, [], ['Inhabilitar']),
+
+(100, 20, 10, 10, 'admin', 100, 10, ['Golpe Chispa'], [])]
+statList = [ 'Vida', 'Ataque', 'Defensa', 'Velocidad', 'Energia','Mana']
+
 
 parameters = { 'advanceEnergyCost': 2, 'encounter':4, 'itemDrop':5 ,
 'posibleDrops':{'pocion de vida':1, 'pocion de energia':1}, 
@@ -14,13 +21,13 @@ itemList = {'pocion de vida': 1, 'pocion de energia': 1, 'pocion de mana': 0}
 
 skillList = {'Golpe Chispa': 5, 'Rafaga de Viento': 9}
 
-tecniqueList = {'Doble Golpe': 5, 'Tornado Infernal': 20}
+tecniqueList = {'Doble Golpe': 5, 'Punto Ciego':6, 'Tiro Bajo':4,'Tornado Infernal': 20}
 
 
 zones = {'Camino del Hierro Negro': 15}
 
 bosses = [
-	{'name':'Cazador de aventureros', 'life': 45,
+	{'name':'Cazador de aventureros', 'life': 20,
 	'attack': 7, 'defence': 7,
 	'speed': 15, 'energy': 25,
 	'aptitude':['great'], 'mana':0,
@@ -30,13 +37,13 @@ bosses = [
 
 
 plainsEnemys = [
-	{'name':'goblin', 'life': 5, 'att': 5, 'def': 3, 'sp': 5,'en': 4, 'aptitude':['normal'],'mana':0,'sk':[],'tecnique':['Doble Golpe']},
-	{'name':'rata', '  life': 3, 'att': 3, 'def': 1, 'sp': 8,'en': 4, 'aptitude':['dumb'],'mana':0,'sk':[],'tecnique':[]},
-	{'name':'slime',   'life': 5, 'att': 3, 'def': 1, 'sp': 3,'en': 4, 'aptitude':['dumb'],'mana':0,'sk':[],'tecnique':[]},
-	{'name':'zombie',  'life': 14, 'att': 2, 'def': 1,'sp': 1,'en': 2, 'aptitude':['dumb'],'mana':0,'sk':[],'tecnique':[]},
+	{'name':'goblin', 'life': 5, 'att': 5, 'def': 3, 'sp': 5,'en': 4, 'aptitude':'normal','mana':0,'sk':'','tecnique':'Doble Golpe'},
+	{'name':'rata', '  life': 3, 'att': 3, 'def': 1, 'sp': 8,'en': 4, 'aptitude':'dumb','mana':0,'sk':'','tecnique':''},
+	{'name':'slime',   'life': 5, 'att': 3, 'def': 1, 'sp': 3,'en': 4, 'aptitude':'dumb','mana':0,'sk':'','tecnique':''},
+	{'name':'zombie',  'life': 14, 'att': 2, 'def': 1,'sp': 1,'en': 2, 'aptitude':'dumb','mana':0,'sk':'','tecnique':''},
 
 	{'name':'aprendiz magico','life': 8 , 'att': 1 , 'def':2,'sp': 10,'en':6,
-	'ap':['magicNormal'], 'mana':7, 'skills':['Golpe Chispa'],'tecnique':[]}
+	'ap':'magicNormal', 'mana':7, 'skills':'Golpe Chispa','tecnique':''}
 ]
 
 # ------------------------ CAMINO DEL HIERRO NEGRO ------------------------------
@@ -87,8 +94,20 @@ attack INTEGER,
 defence INTEGER,
 speed INTEGER,
 class varchar(60),
+states varchar(500),
+mana INTEGER,
+maxMana INTEGER,
+skills varchar(900),
+tecniques varchar(900),
 level INTEGER,
 world varchar(60),
 avalibleEnemys varchar(200),
 days INTEGER
-); SON PUTOS 15 CAMPOS LETS FUCKING GO'''
+); SON PUTOS 19 CAMPOS LETS FUCKING GO
+
+def oldStatusBar(player,enemy, turn):
+	print(f'\n------------------------------- Barra de Status -------------------------------------------',#screen
+		  '\npuntos de vida de ->{}<-: {}/{}  -  energia: {} / {} - Mana:{} / {} - estado:{}'.format( player.name, round(player.life,2), player.maxLife, player.energy, player.maxEnergy,player.mana,player.maxMana, player.states) ,
+		  '\npuntos de vida de ->{}<-: {}/{}  -  energia: {} / {} - Mana:{} / {} - estado:{}\nturno: {}\n'.format(enemy.name, round(enemy.life,  2) , enemy.maxLife, enemy.energy, enemy.maxEnergy, enemy.mana,enemy.maxMana, enemy.states,turn),
+		  f'---------------------------------------------------------------------------------------------\n\n\n\n')#tengo que hacer un salto de pagina
+'''
